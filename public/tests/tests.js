63f02fc2ed585e423e0d7502277f90dcc637ff1e
Guillermo Rivero <alu0100702149.ull.edu.es>
2014-03-20 10:17:43 +0000

    Cambios test

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -6,7 +6,7 @@ suite('Pruebas para los tokens', function(){
   test('Pruebas funcion string', function(){
     var input = "var a = b;";
     var esperado = "[{\"type\":\"ID\",\"value\":\"var\",\"from\":0,\"to\":3},{\"type\":\"ID\",\"value\":\"a\",\"from\":4,\"to\":5},{\"type\":\"=\",\"value\":\"=\",\"from\":6,\"to\":7},{\"type\":\"ID\",\"value\":\"b\",\"from\":8,\"to\":9},{\"type\":\";\",\"value\":\";\",\"from\":9,\"to\":10}]";
-	var resultado = JSON.stringify(tokens(input));
+	var resultado = JSON.stringify(input.tokens());
 
 	assert.equal(esperado, resultado);
   });
